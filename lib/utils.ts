287d255ac1bca3898853865f5f06103c16cf1b2d
Ceyhun <bcatagec@gmail.com>
2024-02-18 14:57:51 +0300

    initial commit

---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+// Will be used to dynamically add classes
+
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
