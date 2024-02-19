edc078b0fc540f3e0b7010b8495e6d55a3190c00
Ceyhun <bcatagec@gmail.com>
2024-02-19 12:18:11 +0300

    login form added

---
+++ app/auth/layout.tsx
@@ -0,0 +1,10 @@
+
+const layout = ({children}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <div className="h-full flex items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-emerald-400 to-teal-600">{children}</div>
+  )
+}
+
+export default layout
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,34 @@
 
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils"
+
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+
 export default function Home() {
-  return (
-   <p className="font-bold text-red-600">Hello Auth!</p>
-  );
+  return <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-emerald-400 to-teal-600">
+    <div className="space-y-6 text-center">
+        <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md",
+        font.className)}>
+          🔒Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication service
+        </p>
+
+        <div>
+          <LoginButton>
+          <Button variant="secondary">
+            Sign in
+          </Button>
+          </LoginButton>
+        </div>
+    </div>
+
+  </main>;
 }
